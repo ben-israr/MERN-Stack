c2747902c390ef40c1f2e3f8834a828d720e9356
ben-israr <israrahmedkoondhar123@gmail.com>
2026-08-29 08:41:36 +0500

    Create personal profile web page

---
+++ Demo.js
@@ -0,0 +1,9 @@
+let array=['Israr Ahmed','Koondhar','Sahab'];
+for(let val of array){
+    // console.log(val);
+}
+
+let obj={name:'Israr Ahmed',age:20}
+for(let key in obj){
+    console.log(key,':',obj[key]);
+}
